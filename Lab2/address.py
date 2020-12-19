0214faf6168501a57d005f402482e8c96a1f6e2f
Glebova Anna <35535292+ritchann@users.noreply.github.com>
2020-12-19 12:07:39 +0300

    add tests

--- Lab2/address.py
+++ Lab2/address.py
@@ -11,6 +11,7 @@ from yargy import (
     rule,
     or_,
 )
+from yargy.pipelines import morph_pipeline
 
 City = fact(
     'City',
@@ -24,7 +25,7 @@ Appart = fact(
 
 Building = fact(
     'Building',
-    ['buildingName', 'buildingType']
+    ['buildingName', 'buildingType', 'streetName']
 )
 
 Body = fact(
@@ -32,14 +33,20 @@ Body = fact(
     ['bodyName', 'bodyType']
 )
 
+Structure = fact(
+    'Structure',
+    ['structureName', 'structureType']
+)
+
+Street = fact(
+    'Street',
+    ['streetName', 'streetType']
+)
+
 AddrPart = fact(
     'AddrPart',
     ['value']
 )
-
-defaultCity = City(nameCity=None)
-
-defaultAppart = Appart(appartName=None, typeAppart=None)
 
 def value(key):
     @property
@@ -51,10 +58,16 @@ class City(City):
     value = value('nameCity')
 
 class Appart(Appart):
-        value = value('appartName')
+    value = value('appartName')
+
+class Structure(Structure):
+    value = value('structureName')
+
+class Street(Street):
+    value = value('streetName')
 
 class Building(Building):
-        value = value('buildingName')
+    value = value('buildingName')
 
 class AddrPart(AddrPart):
     @property
@@ -68,11 +81,19 @@ INT = type('INT')
 
 LETTER = in_caseless(set('абвгдежзиклмнопрстуфхшщэюя'))
 
-TYPE = dictionary({
+TYPE_CITY = dictionary({
     'город'
 }).interpretation(
     City.typeCity
 )
+
+STRUCTURE_TYPE = dictionary({
+    'строение',
+    'ст'
+}).interpretation(
+    Structure.structureType
+)
+
 
 TYPE_APPART = dictionary({
     'квартира'
@@ -83,7 +104,8 @@ TYPE_APPART = dictionary({
 BUILDING_TYPE = dictionary({
     'дом',
     'шоссе',
-    'проспект'
+    'проспект',
+    'улица'
 }
 ).interpretation(
     Building.buildingType
@@ -97,14 +119,13 @@ VALUE = rule(
 SEP = in_(r'/\-')
 
 BUILDING_VALUE = or_(
-    rule(VALUE),
-    rule(VALUE, VALUE),
     rule(VALUE, LETTER),
+    rule(VALUE)
 ).interpretation(
     Building.buildingName
 )
 
-STREET = dictionary({
+STREET_VALUE = dictionary({
     'комсомольский',
     'катукова',
     'доватора',
@@ -113,12 +134,43 @@ STREET = dictionary({
     'алтуфьевское',
     'миттова',
     'школьная',
-    'юрия гагарина',
-    'гагарина',
-    'юнтоловский'
+    'рабочая',
+    'юнтоловский',
+    'школьная',
+    'меркулова'
 })
+COMPLEX_STREET = morph_pipeline([
+    'юрия гагарина'
+])
+
+COMPLEX = morph_pipeline([
+    'санкт-петербург',
+    'нижний новгород',
+    'н.новгород',
+    'ростов-на-дону',
+    'набережные челны',
+    'улан-удэ',
+    'нижний тагил',
+    'комсомольск-на-амуре',
+    'йошкар-ола',
+    'старый оскол',
+    'великий новгород',
+    'южно-сахалинск',
+    'петропавловск-камчатский',
+    'каменск-уральский',
+    'орехово-зуево',
+    'сергиев посад',
+    'новый уренгой',
+    'ленинск-кузнецкий',
+    'великие луки',
+    'каменск-шахтинский',
+    'усть-илимск',
+    'усолье-сибирский',
+    'кирово-чепецк',
+])
 
 NAME = dictionary({
+    'санкт-петербург',
     'москва',
     'тольятти',
     'барнаул',
@@ -138,9 +190,7 @@ NAME = dictionary({
     'сургут',
     'липецк',
     'кстово'
-}).interpretation(
-    City.nameCity
-)
+})
 
 BODY_TYPE = dictionary({
     'корпус',
@@ -156,10 +206,23 @@ BODY = or_(
     Body
 )
 
+STRUCTURE = or_(
+    rule(STRUCTURE_TYPE,  INT.interpretation(Structure.structureName))
+).interpretation(
+    Structure
+)
+
 
 CITY = or_(
-    rule(NAME),
-    rule(TYPE, NAME)
+    rule(NAME.interpretation(
+        City.nameCity
+    )),
+    rule(TYPE_CITY, NAME.interpretation(
+        City.nameCity
+    )),
+    COMPLEX.interpretation(
+        City.nameCity
+    )
 ).interpretation(
     City
 )
@@ -172,23 +235,33 @@ APPART = or_(
 
 BUILDING = or_(
     rule(BUILDING_TYPE, BUILDING_VALUE),
-    rule(STREET, BUILDING_VALUE)
+    rule(BUILDING_TYPE, STREET_VALUE.interpretation(Building.streetName), BUILDING_VALUE),
+    rule(STREET_VALUE.interpretation(Building.streetName), BUILDING_VALUE),
+    rule(COMPLEX_STREET.interpretation(Building.streetName), BUILDING_VALUE),
 ).interpretation(
     Building
 )
 
+STREET = or_(
+    rule(STREET_VALUE.interpretation(Street.streetName)),
+    rule(COMPLEX_STREET.interpretation(Street.streetName))).interpretation(
+    Street
+)
+
 ADDR_PART = or_(
-    BUILDING,
     CITY,
+    STREET,
+    BUILDING,
     APPART,
-    BODY
+    BODY,
+    STRUCTURE,
 ).interpretation(
     AddrPart.value
 )
 
 parser = Parser(ADDR_PART)
 
-test = 'питер гагарина 22 к2'
+test =  'улица меркулова дом 24'
 
 # print(parser.findall(test))
 for matc in parser.findall(test):
@@ -208,7 +281,7 @@ class TestCity(unittest.TestCase):
         res = (match.nameCity if hasattr(match, 'nameCity') else None, match.typeCity if hasattr(match, 'typeCity') else None)
         self.assertEqual(res, ('липецк', 'город'))
 
-#
+    #
     def test_3(self):
         testing_address = 'сургут улица рабочая дом 31а'
         match = (None, None)  if parser.find(testing_address) is None else parser.find(testing_address).fact
@@ -434,6 +507,128 @@ class TestCity(unittest.TestCase):
             building = match.fact.buildingName if hasattr(match.fact, 'buildingName') else building
             body = match.fact.bodyName if hasattr(match.fact, 'bodyName') else body
         self.assertEqual((building, body, None), ('78', None, None))
+
+    def test_36(self):
+        testing_address = 'санкт-петербург школьная 20'
+        building = None
+        body = None
+        structure = None
+        city = None
+        street = None
+        for match in parser.findall(testing_address):
+            city = match.fact.nameCity if hasattr(match.fact, 'nameCity') else city
+            building = match.fact.buildingName if hasattr(match.fact, 'buildingName') else building
+            body = match.fact.bodyName if hasattr(match.fact, 'bodyName') else body
+            structure = match.fact.structureName if hasattr(match.fact, 'structureName') else structure
+            street = match.fact.streetName if hasattr(match.fact, 'streetName') else street
+        self.assertEqual(('санкт-петербург', None), (city, None))
+        self.assertEqual(('школьная', None), (street, None))
+        self.assertEqual(('20', None, None), (building, body, structure))
+
+    def test_37(self):
+        testing_address = 'санкт-петербург юрия гагарина 22 к2'
+        building = None
+        body = None
+        structure = None
+        city = None
+        street = None
+        for match in parser.findall(testing_address):
+            city = match.fact.nameCity if hasattr(match.fact, 'nameCity') else city
+            building = match.fact.buildingName if hasattr(match.fact, 'buildingName') else building
+            body = match.fact.bodyName if hasattr(match.fact, 'bodyName') else body
+            structure = match.fact.structureName if hasattr(match.fact, 'structureName') else structure
+            street = match.fact.streetName if hasattr(match.fact, 'streetName') else street
+        self.assertEqual(('санкт-петербург', None), (city, None))
+        self.assertEqual(('юрия гагарина', None), (street, None))
+        self.assertEqual(('22', '2', None), (building, body, structure))
+
+    def test_38(self):
+        testing_address = "санкт-петербург;юнтоловский 43 корпус 1"
+        building = None
+        body = None
+        structure = None
+        city = None
+        street = None
+        for match in parser.findall(testing_address):
+            city = match.fact.nameCity if hasattr(match.fact, 'nameCity') else city
+            building = match.fact.buildingName if hasattr(match.fact, 'buildingName') else building
+            body = match.fact.bodyName if hasattr(match.fact, 'bodyName') else body
+            structure = match.fact.structureName if hasattr(match.fact, 'structureName') else structure
+            street = match.fact.streetName if hasattr(match.fact, 'streetName') else street
+        self.assertEqual(('санкт-петербург', None),  (city, None))
+        self.assertEqual(('юнтоловский', None), (street, None))
+        self.assertEqual(('43', '1', None),  (building, body, structure))
+
+
+    def test_39(self):
+        testing_address = "санкт-петербург;юнтоловский 43 строение 1"
+        building = None
+        body = None
+        structure = None
+        city = None
+        street = None
+        for match in parser.findall(testing_address):
+            city = match.fact.nameCity if hasattr(match.fact, 'nameCity') else city
+            building = match.fact.buildingName if hasattr(match.fact, 'buildingName') else building
+            body = match.fact.bodyName if hasattr(match.fact, 'bodyName') else body
+            structure = match.fact.structureName if hasattr(match.fact, 'structureName') else structure
+            street = match.fact.streetName if hasattr(match.fact, 'streetName') else street
+        self.assertEqual(('санкт-петербург', None), (city, None))
+        self.assertEqual(('юнтоловский', None), (street, None))
+        self.assertEqual(('43',  None, '1'),  (building, body, structure))
+
+    def test_40(self):
+        testing_address = "юнтоловский 43 ст 1"
+        building = None
+        body = None
+        structure = None
+        city = None
+        street = None
+        for match in parser.findall(testing_address):
+            city = match.fact.nameCity if hasattr(match.fact, 'nameCity') else city
+            building = match.fact.buildingName if hasattr(match.fact, 'buildingName') else building
+            body = match.fact.bodyName if hasattr(match.fact, 'bodyName') else body
+            structure = match.fact.structureName if hasattr(match.fact, 'structureName') else structure
+            street = match.fact.streetName if hasattr(match.fact, 'streetName') else street
+        self.assertEqual(('юнтоловский', None), (street, None))
+        self.assertEqual(('43',  None, '1'), (building, body, structure))
+
+    def test_41(self):
+        testing_address = 'проспект комсомольский 50'
+        buildingValue = None
+        buildingType = None
+        for match in parser.findall(testing_address):
+            buildingType = match.fact.buildingType if hasattr(match.fact, 'buildingType') else buildingType
+            buildingValue = match.fact.streetName if hasattr(match.fact, 'streetName') else buildingValue
+        self.assertEqual(('комсомольский', 'проспект'), (buildingValue, buildingType))
+
+    def test_42(self):
+        testing_address = 'город липецк улица катукова 36 a'
+        buildingValue = None
+        buildingType = None
+        for match in parser.findall(testing_address):
+            buildingType = match.fact.buildingType if hasattr(match.fact, 'buildingType') else buildingType
+            buildingValue = match.fact.streetName if hasattr(match.fact, 'streetName') else buildingValue
+        self.assertEqual(('катукова', 'улица'), (buildingValue, buildingType))
+
+
+    def test_43(self):
+        testing_address = 'город липецк доватора 18'
+        buildingValue = None
+        buildingType = None
+        for match in parser.findall(testing_address):
+            buildingType = match.fact.buildingType if hasattr(match.fact, 'buildingType') else buildingType
+            buildingValue = match.fact.streetName if hasattr(match.fact, 'streetName') else buildingValue
+        self.assertEqual(('доватора', None), (buildingValue, buildingType))
+
+    def test_44(self):
+        testing_address = 'ну бехтеева 9 квартира 310'
+        buildingValue = None
+        buildingType = None
+        for match in parser.findall(testing_address):
+            buildingType = match.fact.buildingType if hasattr(match.fact, 'buildingType') else buildingType
+            buildingValue = match.fact.streetName if hasattr(match.fact, 'streetName') else buildingValue
+        self.assertEqual(('бехтеева', None), (buildingValue, buildingType))
 
 
 if __name__ == '__main__':
